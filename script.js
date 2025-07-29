b32e246c31697f952e1cbf60eacf66bab3c806bc
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2025-07-29 20:14:35 +1000

    Tweaks

--- script.js
+++ script.js
@@ -276,9 +276,9 @@ function getDateTime() {
 function writeAngleToWheels(beta) {
     // So lazy
     const speed1 = 90;
-    const speed2 = speed1 - 20;
+    const speed2 = speed1 - 10;
     const speed3 = speed2 - 10;
-    const speed4 = speed3 - 30;
+    const speed4 = speed3 - 40;
     let speed = 0;
     if (beta < 90 + settings.balanceBuffer && beta > 90 - settings.balanceBuffer) {
         // Balanced. Motors stop.
